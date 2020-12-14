e687cf6c972618e506f74017048e8487fc5a3689
guohaoran <2226965642@qq.com>
2020-12-14 23:18:43 +0800

    init

---
+++ src/views/home/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="home">
+  </div>
+</template>
+
+<script>
+
+
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    components: {
+
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
